b857875b27b550916a9e58608329b6de45353f69
kobem28 <aprieyudha.bima@gmail.com>
2022-07-24 18:41:00 +0700

    pertama

---
+++ mapel.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- fontawesome -->
+    <script src="https://kit.fontawesome.com/9bab502e6f.js" crossorigin="anonymous"></script>
+
+    <title>Smanpar Akademik</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/LOGO-SMAN.jpg.png" type="image/x-icon">
+
+</head>
+
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar - Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                <div class="sidebar-brand-icon ">
+                  <img src="img/LOGO-SMAN.jpg.png" alt="" class="w-100">
+                </div>
+                <div class="sidebar-brand-text mx-3">SMANPAR AKADEMIK</div>
+            </a>
+
+            <!-- Divider -->
+            <hr class="sidebar-divider my-0">
+
+            <!-- Nav Item - Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">
+                    <i class="fa-solid fa-user"></i>
+                    <span>Dashboard</span></a>
+            </li>
+            <hr class="sidebar-divider">
+            <li class="nav-item active">
+                <a class="nav-link" href="mapel.php">
+                    <i class="fa-solid fa-book-open"></i>
+                    <span>Mata Pelajaran</span></a>
+            </li>
+            <hr class="sidebar-divider">
+            <li class="nav-item">
+                <a class="nav-link" href="nilai.php">
+                    <i class="fa-solid fa-chart-column"></i>
+                    <span>Nilai</span></a>
+            </li>
+            <hr class="sidebar-divider">
+            <li class="nav-item">
+                <a class="nav-link" href="kartu.php">
+                    <i class="fa-solid fa-newspaper"></i>
+                    <span>Kartu Ujian</span></a>
+            </li>
+            <hr class="sidebar-divider">
+            <li class="nav-item">
+                <a class="nav-link" href="bayar.php">
+                    <i class="fa-solid fa-wallet"></i>
+                    <span>Bayaran</span></a>
+            </li>
+
+        </ul>
+        <!-- End of Sidebar -->
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">   
+
+            <!-- Main Content -->
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow position-relative">
+                    <div class="container mt-2 position-absolute top-0 end-0">
+                        
+                        <div class="nav-item "></div>
+                        <div class="nav-item "></div>
+                        <div class="nav-item ">
+                            <h4>nama</h4> 
+                            <p>nis | <a href="">Logout</a></p>
+                        </div>
+                        
+                    </div>
+                </nav>
+                <!-- content -->
+                <div class="container">
+                    <form class="cari d-flex ">
+                    <div class="col-auto">
+                        <label for="select" class="col-form-label">Semester :</label>
+                    </div>
+                    <select id="select" class="form-select">
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                        <button class="btn btn-primary ml-3">Cari</button>
+                    </form>
+                
+                </div>
+                
+
+
+                <!-- end content -->
+
+                
+            </div>
+            <footer class="sticky-footer bg-white">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy;SMANPAR AKADEMIK 2022</span> <br>
+                        <span>Created by Bima Aprie Yudha</span>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <!-- End of Page Wrapper -->
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
+
+    
+
+</body>
+
+</html>
